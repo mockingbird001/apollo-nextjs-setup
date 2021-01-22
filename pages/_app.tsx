95dd3f75261f841cd1c9d86832e17343b5679e26
Mockingbird <hide0mockingbird@gmail.com>
2021-01-22 19:54:20 +0700

    del ;

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,7 +7,7 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <ApolloProvider client={client}>
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
     </ApolloProvider>
   );
 }
